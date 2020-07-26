03595c2b726fca07fb6b1f3202dec1c34c3f2385
David Elkind <david.elkind@gmail.com>
2020-07-26 17:17:18 +0300

    Predictions arranged by coordinates; full scan reconstruction

--- process_predicted_crops.py
+++ process_predicted_crops.py
@@ -5,7 +5,8 @@ import pickle
 
 import cv2
 import numpy as np
-
+from detectron2.utils.visualizer import GenericMask
+from shapely.geometry import Polygon
 
 source = """
 {
@@ -77,20 +78,20 @@ def create_file_entry(file_path, annotations):
     return current
 
 
-def build_project(crops, outImages, process, suffix):
-    os.makedirs(outImages, exist_ok=True)
+def build_project(crops, output_dir, process, suffix):
+    os.makedirs(output_dir, exist_ok=True)
     project = json.loads(source)
     metadata = dict()
-    for crop, coords, polygons in crops:
+    for crop, coords, prediction in crops:
         processed = process(crop)
-        file_name = f'{coords[0]}_{coords[1]}_{suffix}.jpg'
-        image_path = os.path.join(outImages, file_name)
+        file_name = f'{coords[1]}_{coords[0]}_{suffix}.jpg'
+        image_path = os.path.join(output_dir, file_name)
         cv2.imwrite(image_path, processed)
-        entry = create_file_entry(image_path, polygons)
+        entry = create_file_entry(image_path, to_polygons(prediction))
         entry['size'] = os.path.getsize(image_path)
         metadata[entry['filename'] + str(entry['size'])] = entry
     project['_via_img_metadata'] = metadata
-    with open(os.path.join(outImages, 'via.json'), 'wt') as out:
+    with open(os.path.join(output_dir, 'via.json'), 'wt') as out:
         json.dump(project, out, indent=4, sort_keys=True)
 
 
@@ -99,17 +100,63 @@ def file_without_ext(path):
 
 
 def concat_eq(crop):
-    return np.concatenate(
-        [cv2.cvtColor(cv2.equalizeHist(cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)), cv2.COLOR_GRAY2BGR), crop], axis=1)
+    return np.concatenate([cv2.equalizeHist(crop), crop], axis=1)
+
+
+def to_polygons(predictions):
+    boxes = predictions.pred_boxes if predictions.has("pred_boxes") else None
+    scores = predictions.scores if predictions.has("scores") else None
+    classes = predictions.pred_classes if predictions.has("pred_classes") else None
+    keypoints = predictions.pred_keypoints if predictions.has("pred_keypoints") else None
+
+    if not predictions.has('pred_masks'):
+        return None
+
+    masks = np.asarray(predictions.pred_masks)
+    masks = [GenericMask(x, 312, 312) for x in masks]
+    polygons = [poly.reshape(-1, 2) for mask in masks for poly in mask.polygons]
+    return polygons
+
+
+def create_annotated_scan(crops, output_dir, suffix):
+    coord_lists = list(zip(*[coords for _, coords, _ in crops]))
+    size_y = max(coord_lists[0]) + 312
+    size_x = max(coord_lists[1]) + 312
+    original = np.zeros((size_y, size_x, 3), dtype=np.int16)
+    polygons = []
+    for crop, coords, pred in crops:
+        crop = cv2.cvtColor(crop, cv2.COLOR_GRAY2BGR)
+        polygons += [poly + list(reversed(coords)) for poly in to_polygons(pred)]
+        original[coords[0]:coords[0] + 312, coords[1]: coords[1] + 312, :] = crop
+
+    cv2.polylines(original, polygons, isClosed=True, color=(0, 255, 0))
+    cv2.imwrite(os.path.join(output_dir, f'{suffix}.jpg'), original)
+
+
+def coords_to_str(crop):
+    return f'{crop[0][1][1]}_{crop[0][1][0]}-{crop[-1][1][1]}_{crop[-1][1][0]}'
+    pass
+
+
+def group_by_rows(crops):
+    ycoords = set(crop[1][0] for crop in crops)
+    sorted_coords = sorted([crop[1] for crop in crops if crop[1][0] == val] for val in ycoords)
+    crops = {crop[1]: crop for crop in crops}
+    crops = [[crops[coord] for coord in coord_list] for coord_list in sorted_coords]
+    return crops
 
 
 def main(predictions, output_dir):
     with open(predictions, 'rb') as f:
         predictions = pickle.load(f)
+        os.makedirs(output_dir, exist_ok=True)
         for full_scan, crops in predictions.items():
             suffix = file_without_ext(full_scan)
-            build_project(crops, f'{output_dir}/{suffix}/simple', lambda c: c, suffix)
-            build_project(crops, f'{output_dir}/{suffix}/augmented', concat_eq, suffix)
+            create_annotated_scan(crops, output_dir, suffix)
+            crops = group_by_rows(crops)
+            for crop in crops:
+                build_project(crop, f'{output_dir}/{suffix}/simple/{crop[0][1][0]}', lambda c: c, suffix)
+                build_project(crop, f'{output_dir}/{suffix}/augmented/{crop[0][1][0]}', concat_eq, suffix)
 
 
 if __name__ == '__main__':
