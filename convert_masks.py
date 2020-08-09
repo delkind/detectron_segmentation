47e1e0c090e064033de3e0f71a2ec637663936ba
David Elkind <david.elkind@gmail.com>
2020-08-09 16:44:13 +0300

    Edited mask conversion script

--- convert_masks.py
+++ convert_masks.py
@@ -9,14 +9,24 @@ from tqdm import tqdm
 from predict_experiment import get_downloaded_experiments
 
 
-def main(input_dir):
+def verify_conversion(ctrs, mask):
+    print(f"Source contours shape: {ctrs.shape}")
+    trial_mask = np.zeros_like(mask)
+    ctrs1, _ = cv2.findContours(cv2.cvtColor(mask, cv2.COLOR_BGRA2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.fillPoly(trial_mask, ctrs, color=(0, 255, 0, 255))
+    ctrs1, _ = cv2.findContours(cv2.cvtColor(mask, cv2.COLOR_BGRA2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(f"Resulting contours shape: {ctrs1.shape}")
+    return not (ctrs.shape != ctrs1.shape or any(map(lambda c: (c[0] != c[1]).any(), zip(ctrs, ctrs1))))
+
+
+def main(input_dir, verify):
     experiment_ids = get_downloaded_experiments(input_dir)
     for experiment_id in experiment_ids:
         input_dir = f'output/experiments/{experiment_id}'
         results = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
         results = sorted(list({'-'.join(f.split('-')[:2]) for f in results}))
         for result in tqdm(results, desc=f"Processing {experiment_id}..."):
-            filename = f'{input_dir}/{result}-cellmask.png'
+            filename = f'{input_dir}/{result}-mask.png'
             if not isfile(filename):
                 continue
             mask = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
@@ -24,10 +34,8 @@ def main(input_dir):
             mask = np.zeros_like(np.stack([np.zeros_like(mask), mask, np.zeros_like(mask), mask], axis=2),
                                  dtype=np.uint8)
             cv2.polylines(mask, ctrs, True, color=(0, 255, 0, 255))
-            ctrs1, _ = cv2.findContours(cv2.cvtColor(mask, cv2.COLOR_BGRA2GRAY), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            if any(map(lambda c: (c[0] != c[1]).any(), zip(ctrs, ctrs1))):
-                print('No Match')
-
+            if not verify_conversion(ctrs, mask):
+                print(f"Mismatch in {filename}")
             cv2.imwrite(filename, mask)
 
 
@@ -35,5 +43,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Detectron Mask R-CNN for cells segmentation - convert mask file')
     parser.add_argument('input_dir', action='store', help='Directory containing full scan files')
+    parser.add_argument('--verify', action='store_true', help='Verify shapes match')
     args = parser.parse_args()
     main(**vars(args))
