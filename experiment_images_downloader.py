1da8287568b4ef315db90cafef3b87167705b3d9
David Elkind <david.elkind@gmail.com>
2021-03-05 09:14:57 +0200

    Transient errors handling improved

--- experiment_images_downloader.py
+++ experiment_images_downloader.py
@@ -38,7 +38,7 @@ class ExperimentImagesDownloader(DirWatcher):
     def on_process_error(self, item, exception):
         retval = super().on_process_error(item, exception)
         self.logger.error(f"Error occurred during processing", exc_info=True)
-        if type(exception) in [urllib.error.HTTPError, OSError, ValueError, http.client.RemoteDisconnected]:
+        if any(map(lambda x: issubclass(exception, x), [urllib.error.HTTPError, OSError, ValueError, http.client.error])):
             return False
         else:
             return retval
