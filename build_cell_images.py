ef35441ba3a041606d3fdef1e78a451f02e2b56f
David Elkind <david.elkind>
2022-01-13 23:42:19 +0200

    Figure recreated

--- build_cell_images.py
+++ build_cell_images.py
@@ -25,14 +25,13 @@ def build_cell_grid(experiment, data_dir, seg_data_dir, output_path):
                        bboxes,
                        seg_data))
 
-    fig, ax = plt.subplots(figsize=(3 * FIG_SCALE, 2 * FIG_SCALE), dpi=25)
-
     pool = Pool(cpu_count() // 2)
     patches = list(tqdm(pool.imap(build_section, params), "Building sections", total=len(params)))
 
     for i, result in enumerate(tqdm(patches, "Plotting sections", total=len(patches))):
+        fig, ax = plt.subplots(figsize=(3 * FIG_SCALE, 2 * FIG_SCALE), dpi=25)
         brain_bbox, patches = result
-        plot_patch_collection(ax.flatten()[i], brain_bbox, patches)
+        plot_patch_collection(ax, brain_bbox, patches)
         print("Saving figure...")
         fig.savefig(output_path[:output_path.rfind('.')] + f'-{i}' + output_path[output_path.rfind('.'):], dpi=25)
         plt.close()
